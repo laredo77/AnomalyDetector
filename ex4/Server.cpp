e33757ae3ec47b3d024f82a3bb6a9e46c2ecf7b4
laredo <laredo93i@gmail.com>
2021-01-19 16:15:02 +0200

    Update

---
+++ ex4/Server.cpp
@@ -0,0 +1,71 @@
+/*
+ * Server.cpp
+ *
+ * Author: 311547087, Itamar Laredo
+ */
+#include "Server.h"
+
+/*
+ * Constructor of server class.
+ * This function open socket for optional client connection.
+ */
+Server::Server(int port) throw(const char *) {
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        close(fd); // release fd
+        throw "socket failed";
+    }
+
+    m_server.sin_family = AF_INET; // IP version 4
+    m_server.sin_addr.s_addr = INADDR_ANY;
+    m_server.sin_port = htons(port);
+
+    if (bind(fd, (struct sockaddr *) &m_server, sizeof(m_server)) < 0)
+        throw "bind failure";
+
+    if (listen(fd, 3) < 0)
+        throw "listen failure";
+
+    this->should_stop = false;
+}
+
+/*
+ * Waiting for clients. timeout is set for 1 second.
+ * If client connect, the server open new thread the will
+ * handle the client and then it ready to connect new client.
+ */
+void Server::start(ClientHandler &ch) throw(const char *) {
+
+    t = new thread([&ch, this]() { // lambda expression
+        timeval timeout;
+        while (!should_stop) {
+            timeout.tv_sec = 1;
+            timeout.tv_usec = 0;
+            cout << "waiting for client" << endl;
+            socklen_t clientSize = sizeof(this->m_client);
+            int aClient = accept(this->fd, (struct sockaddr *) &this->m_client, &clientSize);
+            if (aClient < 0)
+                throw "accept failure";
+            cout << "client connected" << endl;
+            t = new thread([&ch, aClient]() {
+                ch.handle(aClient);
+                close(aClient);
+            });
+        }
+        close(fd);
+    });
+}
+
+/*
+ * when connection finish, this function will close the
+ * thread and stop accepting clients loop.
+ */
+void Server::stop() {
+    should_stop = true;
+    t->join(); // do not delete this!
+}
+
+Server::~Server() {
+}
+
---
+++ ex4/Server.h
@@ -0,0 +1,107 @@
+/*
+ * Server.h
+ *
+ * Author: 311547087, Itamar Laredo
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <iostream>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <thread>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "CLI.h"
+
+using namespace std;
+
+/*
+ * A socket IO implemented class.
+ * this call implement the socket input and output functions.
+ * If client use the CLI through server the connection will
+ * be via this socket io.
+ */
+class SocketIO: public DefaultIO {
+    int client;
+public:
+    SocketIO(int clientID) {
+        client = clientID;
+    }
+
+    virtual void write(string text) {
+        send(client, text.c_str(), strlen(text.c_str()), 0);
+    }
+
+    virtual void write(float f) {
+        stringstream ss;
+        ss << f;
+        send(client, ss.str().c_str(), ss.str().length(), 0);
+    }
+
+    virtual string read() {
+        string s;
+        char buffer;
+        while (true) {
+            recv(client, &buffer, 1, 0);
+            if (buffer == '\n')
+                break;
+            s += buffer;
+        }
+        return s;
+    }
+
+    virtual void read(float* f){
+        float buffer;
+        recv(client, &buffer, sizeof(buffer), 0);
+    }
+};
+
+/*
+ * handle a client class.
+ */
+class ClientHandler {
+    public:
+    virtual void handle(int clientID)=0;
+};
+
+/*
+ * The server can handle few clients at the same time.
+ * each client get socket io, and then the server calls
+ * cli.start to start the conversation with the client.
+ */
+class AnomalyDetectionHandler:public ClientHandler {
+public:
+    virtual void handle(int clientID) {
+        SocketIO sio(clientID);
+        CLI cli(&sio);
+        cli.start();
+    }
+};
+
+/*
+ * Server class.
+ */
+class Server {
+	thread* t; // the thread to run the start() method in
+    sockaddr_in m_server;
+    sockaddr_in m_client;
+    bool should_stop;
+    int fd; // file descriptor
+public:
+	Server(int port) throw (const char*);
+	virtual ~Server();
+	void start(ClientHandler& ch)throw(const char*);
+	void stop();
+};
+
+#endif /* SERVER_H_ */
